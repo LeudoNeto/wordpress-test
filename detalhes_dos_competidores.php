081436cdfd4954555bcc11037690b62deafda091
Leudo Neto <leudoneto1234@gmail.com>
2022-07-12 17:53:43 -0300

    final

--- detalhes_dos_competidores.php
+++ detalhes_dos_competidores.php
@@ -23,6 +23,9 @@
             <li class="nav-item">
               <a class="nav-link" href="listagem_dos_competidores.php">Competidores</a>
             </li>
+            <li class="nav-item">
+              <a class="nav-link" href="ranking.php">Ranking</a>
+            </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Detalhes
